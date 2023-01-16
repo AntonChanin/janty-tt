2db44379140eb329c1be1fc7a54c896775391b0d
antonvh <antonhaninvl@gmail.com>
2023-01-16 10:26:19 +0300

    первичная настройка проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { useEffect, useRef } from 'react';
+
 import './App.css'
+import './style.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+const socket =  useRef<HTMLDivElement | null>(null)
+
+useEffect(() => {
+  if (!socket.current?.childElementCount) {
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });  
+    map.render();
+  }
+}, [])
+
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="map" ref={socket}></div>
     </div>
   )
-}
+};
 
-export default App
+export default App;
